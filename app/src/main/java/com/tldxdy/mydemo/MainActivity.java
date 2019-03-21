e40680eb48e77beee331b2611e9957b09381a200
tldxdy <747417048@qq.com>
2019-03-21 19:24:22 +0800

    修改配置

--- app/src/main/java/com/tldxdy/mydemo/MainActivity.java
+++ app/src/main/java/com/tldxdy/mydemo/MainActivity.java
@@ -1,13 +1,24 @@
 package com.tldxdy.mydemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tldxdy.base.frame.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getlayoutId() {
+        return R.layout.activity_main;
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
     }
 }
