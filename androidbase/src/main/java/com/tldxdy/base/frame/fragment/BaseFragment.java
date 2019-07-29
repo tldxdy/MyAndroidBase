a5c9a439766ab5015950f5b78ce24ba7c872df29
tldxdy <747417048@qq.com>
2019-07-29 17:08:15 +0800

    修改fragment懒加载

--- androidbase/src/main/java/com/tldxdy/base/frame/fragment/BaseFragment.java
+++ androidbase/src/main/java/com/tldxdy/base/frame/fragment/BaseFragment.java
@@ -13,10 +13,25 @@ import butterknife.Unbinder;
 
 public abstract class BaseFragment extends Fragment {
     protected Unbinder mUnbinder;
+
+    /**
+     * 是否初始化过布局
+     */
+    protected boolean isViewInitiated;
+    /**
+     * 当前界面是否可见
+     */
+    protected boolean isVisibleToUser;
+    /**
+     * 是否加载过数据
+     */
+    protected boolean isDataInitiated;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(getContentViewId(), container, false);
+        initData();
         return view;
     }
 
@@ -29,8 +44,36 @@ public abstract class BaseFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        initData();
+        isViewInitiated = true;
+        //加载数据
+        prepareFetchData();
+
     }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        this.isVisibleToUser = isVisibleToUser;
+        if (isVisibleToUser) {
+            prepareFetchData();
+        }
+    }
+
+
+
+    public void prepareFetchData(){
+        if (isVisibleToUser && isViewInitiated && (!isDataInitiated)) {
+            loadData();
+            isDataInitiated = true;
+        }
+    }
+
+
+    /**
+     * 懒加载
+     */
+    protected abstract void loadData();
+
 
     @Override
     public void onDestroyView() {
